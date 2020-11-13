9f210db7cc45cbe755a041d51c30237b16a2e38c
Massimiliano Giovagnoli <massimiliano.giovagnoli.1992@gmail.com>
2020-11-13 12:31:06 +0100

    Initial commit

---
+++ infra/__main__.py
@@ -0,0 +1,20 @@
+import pulumi
+import pulumi_digitalocean as do
+
+config = pulumi.Config()
+
+# Define a digital ocean cluster
+cluster = do.KubernetesCluster(
+    "brice-live-demo-cluster",
+    node_pool={
+        "name": "worker-pool",
+        "node_count": 2,
+        "size": "s-1vcpu-2gb",
+    },
+    region="lon1",
+    version="1.19.3-do.2"
+)
+
+# Expose the kubeconfig
+pulumi.export("pulumi-kubeconfig", cluster.kube_configs[0]["rawConfig"])
+
